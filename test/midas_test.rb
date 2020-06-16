400e006bdeb35f7e7922e1c877712cf5af086edb
Andrew Kane <andrew@chartkick.com>
2020-06-16 01:54:21 -0700

    Updated test comment

--- test/midas_test.rb
+++ test/midas_test.rb
@@ -9,7 +9,7 @@ class MidasTest < Minitest::Test
     expected = [0, 0, 1, 2, 2, 4, 2, 2, 3, 6]
     assert_elements_in_delta expected, scores[0...10]
 
-    skip "Last value off on Travis (seed issue?)" if ENV["TRAVIS"]
+    skip "Different values on Travis (seed issue?)" if ENV["TRAVIS"]
 
     expected = [307.507233, 469.720490, 215.821609, 236.601303, 258.282837]
     assert_elements_in_delta expected, scores[-5..-1]
@@ -34,7 +34,7 @@ class MidasTest < Minitest::Test
     expected = [0, 0, 1, 2, 2, 4, 2, 2, 3, 6]
     assert_elements_in_delta expected, scores[0...10]
 
-    skip "Last value off on Travis (seed issue?)" if ENV["TRAVIS"]
+    skip "Different values on Travis (seed issue?)" if ENV["TRAVIS"]
 
     expected = [307.507233, 469.720490, 2.492458, 12.942609, 31.100597]
     assert_elements_in_delta expected, scores[-5..-1]
@@ -59,7 +59,7 @@ class MidasTest < Minitest::Test
     expected = [0, 0, 1, 2, 2, 4, 2, 2, 3, 6]
     assert_elements_in_delta expected, scores[0...10]
 
-    skip "Last value off on Travis (seed issue?)" if ENV["TRAVIS"]
+    skip "Different values on Travis (seed issue?)" if ENV["TRAVIS"]
 
     expected = [49.704582, 56.181786, 63.055534, 70.325829, 10.779646]
     assert_elements_in_delta expected, scores[-5..-1]
