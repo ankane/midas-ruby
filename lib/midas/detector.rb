33837f2f5d77e3b765c7e8b8ddcbb31e2e8ad88d
Andrew Kane <andrew@chartkick.com>
2020-06-16 01:43:36 -0700

    DRY threshold code

--- lib/midas/detector.rb
+++ lib/midas/detector.rb
@@ -11,14 +11,15 @@ module Midas
     end
 
     def fit_predict(x)
+      threshold = @threshold || Float::NAN
       result =
         if x.is_a?(String)
-          _fit_predict_file(x, @rows, @buckets, @alpha, @threshold || Float::NAN, @relations, @directed, @seed)
+          _fit_predict_file(x, @rows, @buckets, @alpha, threshold, @relations, @directed, @seed)
         else
           x = Numo::Int32.cast(x) unless x.is_a?(Numo::NArray)
           x = x.cast_to(Numo::Int32) unless x.is_a?(Numo::Int32)
           raise ArgumentError, "Bad shape: #{x.shape}" unless x.rank == 2 && x.shape[1] == 3
-          _fit_predict_str(x.to_binary, @rows, @buckets, @alpha, @threshold || Float::NAN, @relations, @directed, @seed)
+          _fit_predict_str(x.to_binary, @rows, @buckets, @alpha, threshold, @relations, @directed, @seed)
         end
 
       Numo::SFloat.from_binary(result)
