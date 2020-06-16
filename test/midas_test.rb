eb52f6d68b2f6faf2fcbcc5a4b2f157b2818d545
Andrew Kane <andrew@chartkick.com>
2020-06-16 01:33:38 -0700

    Updated tests for default seed

--- test/midas_test.rb
+++ test/midas_test.rb
@@ -2,24 +2,24 @@ require_relative "test_helper"
 
 class MidasTest < Minitest::Test
   def test_works
-    midas = Midas.new(rows: 2, buckets: 1024, seed: 123)
+    midas = Midas.new(buckets: 1024)
     scores = midas.fit_predict(data)
 
     assert_equal [10000], scores.shape
     expected = [0, 0, 1, 2, 2, 4, 2, 2, 3, 6]
     assert_elements_in_delta expected, scores[0...10]
-    expected = [262.725342, 410.009674, 215.821609, 236.601303, 258.282837]
+    expected = [307.507233, 469.720490, 215.821609, 236.601303, 258.282837]
     assert_elements_in_delta expected, scores[-5..-1]
   end
 
   def test_no_relations
-    midas = Midas.new(rows: 2, buckets: 1024, relations: false, seed: 123)
+    midas = Midas.new(buckets: 1024, relations: false)
     scores = midas.fit_predict(data)
 
     assert_equal [10000], scores.shape
     expected = [0, 0, 1, 2, 2, 4, 2, 2, 3, 6]
     assert_elements_in_delta expected, scores[0...10]
-    expected = [262.725342, 410.009674, 2.492458, 12.942609, 31.100597]
+    expected = [307.507233, 469.720490, 2.492458, 12.942609, 31.100597]
     assert_elements_in_delta expected, scores[-5..-1]
   end
 
@@ -35,16 +35,16 @@ class MidasTest < Minitest::Test
   end
 
   def test_file
-    midas = Midas.new(rows: 2, buckets: 1024, seed: 123)
+    midas = Midas.new(buckets: 1024)
     scores = midas.fit_predict("vendor/MIDAS/data/DARPA/darpa_processed.csv")
 
     assert_equal [4554344], scores.shape
     expected = [0, 0, 1, 2, 2, 4, 2, 2, 3, 6]
     assert_elements_in_delta expected, scores[0...10]
 
-    skip "Last value off by 0.012 on Travis (seed issue?)" if ENV["TRAVIS"]
+    # skip "Last value off on Travis (seed issue?)" if ENV["TRAVIS"]
 
-    expected = [49.031101, 55.438084, 62.238354, 69.431908, 10.681725]
+    expected = [49.704582, 56.181786, 63.055534, 70.325829, 10.779646]
     assert_elements_in_delta expected, scores[-5..-1]
   end
 
