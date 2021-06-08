4fca4d71bb010aed8781302ebd7ba759fb365ef7
Andrew Kane <andrew@ankane.org>
2021-06-08 01:27:38 -0700

    Fixed installation error

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
+## 0.3.2 (unreleased)
+
+- Fixed installation error
+
 ## 0.3.1 (2021-05-23)
 
 - Improved performance
--- ext/midas/extconf.rb
+++ ext/midas/extconf.rb
@@ -1,10 +1,11 @@
 require "mkmf-rice"
+require "numo/narray"
 
 $CXXFLAGS << " -std=c++17 $(optflags)"
 
-numo = $LOAD_PATH.find { |v| File.exist?("#{v}/numo/numo/narray.h") }
-abort "Numo header not found" unless numo && find_header("numo/narray.h", "#{numo}/numo")
-abort "Numo library not found" if Gem.win_platform? && !find_library("narray", nil, "#{numo}/numo")
+numo = File.join(Gem.loaded_specs["numo-narray"].require_path, "numo")
+abort "Numo header not found" unless find_header("numo/narray.h", numo)
+abort "Numo library not found" if Gem.win_platform? && !find_library("narray", nil, numo)
 
 midas = File.expand_path("../../vendor/MIDAS/src", __dir__)
 abort "Midas not found" unless find_header("NormalCore.hpp", midas)
