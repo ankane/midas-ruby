a68db2ac6f0a197b612e7559a4f33495315e795e
Andrew Kane <andrew@ankane.org>
2022-12-27 13:47:26 -0800

    Fixed installation error on Mac

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
+## 0.3.3 (unreleased)
+
+- Fixed installation error on Mac
+
 ## 0.3.2 (2021-06-08)
 
 - Fixed installation error
--- ext/midas/extconf.rb
+++ ext/midas/extconf.rb
@@ -7,6 +7,9 @@ numo = File.join(Gem.loaded_specs["numo-narray"].require_path, "numo")
 abort "Numo header not found" unless find_header("numo/narray.h", numo)
 abort "Numo library not found" if Gem.win_platform? && !find_library("narray", nil, numo)
 
+# for https://bugs.ruby-lang.org/issues/19005
+$LDFLAGS += " -Wl,-undefined,dynamic_lookup" if RbConfig::CONFIG["host_os"] =~ /darwin/i
+
 midas = File.expand_path("../../vendor/MIDAS/src", __dir__)
 abort "Midas not found" unless find_header("NormalCore.hpp", midas)
 
